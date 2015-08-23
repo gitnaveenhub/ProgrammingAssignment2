d6ea04ff53ff13493290a7e0b0f6f3d20d2e59b4
Naveen <deepak2064@yahoo.com>
2015-08-24 04:43:56 +0530

    Assignment2 updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+# MakeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+# i have used "InputMat" in the place of "x"
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(InputMat = matrix()) {
+    MatrixInverse <- NULL
+    set <- function(AssignMat) {
+        InputMat <<- AssignMat
+        MatrixInverse <<- NULL
+    }
+    get <- function() InputMat
+    setinverse <- function(inverse) MatrixInverse <<- inverse
+    getinverse <- function() MatrixInverse
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
+# CacheSolve: This function computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix above. If the inverse has already been calculated 
+#(and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(InputMat, ...) {
+    inv <- InputMat$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    MatData <- InputMat$get()
+    MatrixInverse <- solve(MatData)
+    InputMat$setinverse(MatrixInverse)
+    MatrixInverse
 }
+
+#Execution and Output validation
+
+InputMat <- matrix(1:4,2,2)
+GetCacheMat <- makeCacheMatrix(InputMat)
+
+GetCacheMat$get()
+#       [,1] [,2]
+#[1,]    1    3
+#[2,]    2    4
+
+cacheSolve(GetCacheMat) #Run 01
+#       [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
+
+cacheSolve(GetCacheMat) #Run 02
+#getting cached data.
+#       [,1] [,2]
+#[1,]   -2  1.5
+#[2,]    1 -0.5
